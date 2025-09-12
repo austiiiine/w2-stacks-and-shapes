be9955eab79813302569bf0741a341055b55769a
Austine Huang <austine.huang@gmail.com>
2025-09-12 09:49:56 +0800

    Initial Commit

---
+++ w2demo/w2demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  w2demoApp.swift
+//  w2demo
+//
+//  Created by Austine Huang on 2025/9/12.
+//
+
+import SwiftUI
+
+@main
+struct w2demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
